2de1f501e7396c62a62438abee5e71feabab5481
ind0zer <165696434+ind0zer@users.noreply.github.com>
2024-08-19 00:00:02 +0300

    hw

---
+++ main.js
@@ -0,0 +1,40 @@
+const users = [
+  { login: "indozer", balance: 15000 },
+  { login: "lanvin", balance: 10000 },
+  { login: "camel", balance: 20000 },
+];
+
+const sumBalance = users.reduce((acc, num) => {
+    return acc + num.balance
+}, 0);
+
+console.log(sumBalance);
+
+const friends = [
+    {name: 'Artem', friend: 'Makar'},
+    {name: 'Makar', friend: 'Artem'},
+    {name: 'Vlad', friend: 'Makar'}
+]
+
+const selectedFriend = friends.filter((user) => user.friend === 'Makar');
+console.log(selectedFriend);
+
+
+const friends2 = [
+    {name: 'Artem', friend: 6},
+    {name: 'Makar', friend: 9},
+    {name: 'Vlad', friend: 0}
+]
+
+const sortFriend = friends2.sort((a,b) => a.friend - b.friend)
+console.log(sortFriend)
+
+const users2 = [
+    { name: 'Artem', skills: ["JavaScript", "HTML", "CSS"] },
+    { name: 'Makar', skills: ["Python", "CSS", "Django"] },
+    { name: 'Vlad', skills: ["JavaScript", "Node.js", "Python"] }
+];
+
+const filterUsers = users2.flatMap((user) => user.skills).filter((skill, index, array) => array.indexOf(skill) === index).sort()
+
+console.log(filterUsers)
